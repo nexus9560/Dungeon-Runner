d57953d1152a81a12702824cac1d8a2b162391dd
nexus9560 <ke5vra@gmail.com>
2025-05-26 00:09:19 -0500

    Refactor: Move type definitions to DungeonTypes.h
    - Added include directive for "DungeonTypes.h" in Dungeon-Runner.c.
    - Removed struct definitions from Dungeon-Runner.c, relocating them to "DungeonTypes.h".
    - Standardized formatting of directional vector constants.
    - Updated project files to include and categorize "DungeonTypes.h".

--- Dungeon-Runner/Dungeon-Runner.c
+++ Dungeon-Runner/Dungeon-Runner.c
@@ -11,6 +11,7 @@
 #include <ctype.h>
 #include <time.h>
 #include <math.h>
+#include "DungeonTypes.h"
 
 #define XBOUND 256
 #define YBOUND 512
@@ -34,68 +35,6 @@
 #define CLEAR_COMMAND "" // Define it to nothing if OS is not detected
 #endif
 
-typedef struct {
-	int dx;
-	int dy;
-} Dun_Vec;
-
-typedef struct {
-	unsigned int x;
-	unsigned int y;
-} Dun_Coord;
-
-typedef struct{
-	char name[32];
-	char stat[3];
-	int bonus;
-	int type; // 0 = weapon, 1 = armor, 2 = consumable
-	int equipped; // 0 = not equipped, 1 = equipped
-} Item;
-
-typedef enum {
-	WEAPON = 0,
-	ARMOR = 1,
-	CONSUMABLE = 2,
-} ItemType;
-
-typedef struct{
-	Dun_Coord location;
-	char name[32];
-	int health;
-	int curHealth;
-	int atk;
-	int agr;
-	int hit;
-	int def;
-	int exp;
-	int eva;
-	unsigned int level;
-	Dun_Coord stepLog[LOG_BUFFER];
-	unsigned int currentPos;
-} Entity;
-
-typedef struct {
-	Entity base;
-	int stepCount;
-	Item* inventory; // Inventory of items
-} Player;
-
-typedef struct {
-	Dun_Coord startLocation; // Corner location in the world map, as well as the initial offset
-	unsigned int xdim;
-	unsigned int ydim;
-
-} Room;
-
-typedef struct{
-	int locationID;
-	char ref;
-	int passable; // 0 = not passable, 1 = passable
-	int occupied; // 0 = not occupied, 1 = occupied
-} Cell;
-
-
-
 Cell world[XBOUND][YBOUND];
 Player you;
 Entity* enemyGlossary;
@@ -103,10 +42,10 @@ Entity* enemiesOnFloor; // Took the Entities array out of Room, because Entities
 int enemyGlossarySize;
 unsigned int roomCount;
 Item itemGlossary[MAX_ITEMS];
-const Dun_Vec up	= { -1,  0 };
-const Dun_Vec right = {  0,  1 };
-const Dun_Vec down	= {  1,  0 };
-const Dun_Vec left	= {  0, -1 };
+const Dun_Vec up = { -1,  0 };
+const Dun_Vec right = { 0,  1 };
+const Dun_Vec down = { 1,  0 };
+const Dun_Vec left = { 0, -1 };
 Room* rooms;
 unsigned int currRoomCount = 0;
 
