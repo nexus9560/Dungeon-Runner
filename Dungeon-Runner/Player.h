cabeb8dfb84d72e9c8d6dd68e6718994ed6a31a2
nexus9560 <ke5vra@gmail.com>
2025-08-14 01:42:53 -0500

    Adjusted an item description, and began setting up player for modifications.

--- Dungeon-Runner/Player.h
+++ Dungeon-Runner/Player.h
@@ -7,6 +7,20 @@
 
 DR_LIST_DEF(Item)
 
+typedef Item__List IL;
+
+typedef struct {
+	unsigned int arm, left; // if arm == 1 -> arm else leg, if left == 1 -> left else right
+	Item armor;
+	Item weapon;
+
+} Limb;
+
+DR_LIST_DEF(Limb)
+
+typedef Limb__List Limblist;
+
+
 typedef struct {
 	Entity base;
 	int stepCount;
@@ -14,5 +28,17 @@ typedef struct {
 } Player;
 
 extern Player you;
+
+int player_init(void);
+
+int add_Limb(Limb* limb);
+
+int remove_Limb(Limb* limb);
+
+int add_Item(Item* item);
+
+int remove_Item(Item* item);
+
+int equip_Item_to_Limb(Item* item, Limb* limb);
 
 #endif
