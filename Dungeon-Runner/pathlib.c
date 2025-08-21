ff1c66dc41a9ca1a3e7f73f05947a1c53b68cac1
nexus9560 <ke5vra@gmail.com>
2025-08-21 00:48:43 -0500

    Continued fixing things

--- Dungeon-Runner/pathlib.c
+++ Dungeon-Runner/pathlib.c
@@ -16,16 +16,32 @@ void path__join(const char *path1, const char *path2, char *destination) {
 
     if (destination[len - 1] == PATHLIB_DIR_SEPARATOR) {
       if (path2 && *path2) {
+#ifdef _WIN32
         strcpy_s(destination + len, PATHLIB_MAX_PATH - len, (*path2 == PATHLIB_DIR_SEPARATOR) ? (path2 + 1) : path2);
+#else 
+        strncpy(destination + len, (*path2 == PATHLIB_DIR_SEPARATOR) ? (path2 + 1) : path2, PATHLIB_MAX_PATH - len - 1);
+		destination[PATHLIB_MAX_PATH - 1] = '\0';
+#endif
       }
     }
     else {
-      if (path2 && *path2) {
-        if (*path2 == PATHLIB_DIR_SEPARATOR)
-          strcpy_s(destination + len, PATHLIB_MAX_PATH - len, path2);
+        if (path2 && *path2) {
+            if (*path2 == PATHLIB_DIR_SEPARATOR) {
+#ifdef _WIN32
+                strcpy_s(destination + len, PATHLIB_MAX_PATH - len, path2);
+#else
+				strncpy(destination + len, path2, PATHLIB_MAX_PATH - len - 1);
+				destination[PATHLIB_MAX_PATH - 1] = '\0';
+#endif
+            }
         else {
           destination[len] = PATHLIB_DIR_SEPARATOR;
+#ifdef _WIN32
           strcpy_s(destination + len + 1, PATHLIB_MAX_PATH - len - 1, path2);
+#else
+          strncpy(destination + len + 1, path2, PATHLIB_MAX_PATH - len - 2);
+		  destination[PATHLIB_MAX_PATH - 1] = '\0';
+#endif
         }
       }
     }
